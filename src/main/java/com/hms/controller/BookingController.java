44bb41ac39860dca28c435e377dacd02c96f2d70
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2024-02-06 12:40:33 -0700

    Adding permission in maven.yml Github Action

--- src/main/java/com/hms/controller/BookingController.java
+++ src/main/java/com/hms/controller/BookingController.java
@@ -5,7 +5,6 @@ import com.hms.model.Booking;
 import com.hms.service.BookingService;
 import com.hms.utils.ApiUrls;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
