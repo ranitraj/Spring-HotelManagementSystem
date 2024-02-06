e637b60b063f375c34f65acc794db7f82746adb0
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2024-02-05 21:27:57 -0700

    Adding Booking model class

---
+++ src/main/java/com/hms/model/Booking.java
@@ -0,0 +1,32 @@
+package com.hms.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+    @Id
+    @SequenceGenerator(
+            name = "booking_sequence",
+            sequenceName = "booking_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "booking_sequence"
+    )
+    private long id;
+    private String customerName;
+    private String roomType;
+    private Date checkInDate;
+    private Date checkOutDate;
+    private float totalPrice;
+}
