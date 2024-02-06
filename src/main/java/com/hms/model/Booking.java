302b8911df943e56a5d0b3951df19ada0ee4f381
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2024-02-06 10:49:31 -0700

    Modifying Booking model class to add vaidations and formatters

--- src/main/java/com/hms/model/Booking.java
+++ src/main/java/com/hms/model/Booking.java
@@ -1,6 +1,9 @@
 package com.hms.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,9 +27,15 @@ public class Booking {
             generator = "booking_sequence"
     )
     private Long id;
+    @NotNull(message = "Customer name cannot be null")
     private String customerName;
+    @NotNull(message = "Room type cannot be null")
+
     private String roomType;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date checkInDate;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date checkOutDate;
+    @DecimalMin(value = "0.0", inclusive = false, message = "Total price must be greater than 0")
     private float totalPrice;
 }
